20fa2121767ce19da86b85d6b501e5a6eddfff99
Angel Cervera Ronda <angelcerveraronda@gmail.com>
2025-04-05 15:40:07 +0200

    charcters.cpp
    added the file but needs to be tested

--- characters.cpp
+++ characters.cpp
@@ -1,13 +1,33 @@
 using namespace std;
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 
-class Character{
+class Characters{
 
-    //Atributes
+    //Attributes
     public:
-        float health;
+
+        int health;
+        int armour;
+        int initiative;
+        int strength;
+        int dexterity;
+        int constitution;
+        int intelligence;
+        int wisdom;
+        int charm;
+
+        vector<float> inventory[10];
+
+        int money;
 
     //Functions
-    
+    public:
+
+        int dado(int caras){
+            int total = rand()%caras+1;
+            return total;
+        }
 
 };
