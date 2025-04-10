b4353f99ef03a8240c9af72eb93d846f8bf74115
Angel Cervera Ronda <angelcerveraronda@gmail.com>
2025-04-10 16:04:10 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,53 @@
-using namespace std;
 #include <iostream>
-#include "header.h"
+#include <vector>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+//Classes
+class Characters{
+
+    //Attributes
+    public:
+        
+        string name;
+        int health;
+        int armour;
+        int initiative;
+        int strength;
+        int dexterity;
+        int constitution;
+        int intelligence;
+        int wisdom;
+        int charm;
+        int race;
+        int clas;
+
+        int money;
+
+    //Functions
+    public:
+
+        int dado(int caras){
+            int total = rand()%caras+1;
+            return total;
+        }
+
+};
+
+//Global atributes
+
+//Main
 
 int main(){
 
-Characters p1;
-p1.name = "paco";
+    srand(time(NULL));
 
-cout << "Tu nombre es " << p1.name;
-int tirada = p1.dado(6);
-cout << tirada;
+    Characters p1;
+    p1.name = "paco";
+
+    cout << "Tu nombre es " << p1.name;
+    int tirada = p1.dado(6);
+    cout << tirada;
 }
